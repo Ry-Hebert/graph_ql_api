ab85fcc1b0aab74353abe8a6505a14dcbfba2788
Ryan <ryan@thecpafirm.com>
2021-05-04 21:26:00 -0600

    Gql

--- prisma/seed/seed.js
+++ prisma/seed/seed.js
@@ -4,17 +4,33 @@ const favorites_categories = require('./sc-favorites-categories.json')
 const prisma = new PrismaClient()
 
 async function loadCategories(){
-    const allCategories = favorites_categories[]
+    const allCategories = favorites_categories['comet'].seedData
+        return allCategories.map((category) =>{
+            return{
+                data: {
+                    name: category.name,
+                    maxSize: category.maxSize,
+                    favorites: category.favorites
+                }
+            }
+        })
 }
 
 async function main() {
     const categories = await loadCategories()
+    for (const category of categories){
+        try{
+            await prisma.seedData.create(category)
+        } catch (error) {
+            console.log(`Error creating Categories: ${error}`)
+        }
+    }
 }
 
 main()
-.catch((e) => {
-    throw e
-})
-.finally(async () =>{
+    .catch((e) => {
+        throw e
+    })
+    .finally(async () =>{
     await prisma.$disconnect()
 })
